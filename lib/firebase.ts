83796d47527b37e1c76c73b2d84a6b78a067bebd
Sabith-code <muhammedsabith221b@gmail.com>
2025-08-19 20:15:25 +0530

    bug fixess

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,6 +2,16 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
+
+// Required Vercel env variables:
+// NEXT_PUBLIC_FIREBASE_API_KEY
+// NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN
+// NEXT_PUBLIC_FIREBASE_PROJECT_ID
+// NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET
+// NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID
+// NEXT_PUBLIC_FIREBASE_APP_ID
+// NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID (optional)
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
@@ -9,6 +19,7 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
